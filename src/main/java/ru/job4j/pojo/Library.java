39a01bc9ff4332317be5495ee42c3069f80fcea3
KhvanVasilii <hvanvasya@gmail.com>
2022-03-12 14:00:12 +0900

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book headFirst = new Book(200, "Изучаем Java");
+        Book effectiveJava = new Book(300, "Java, Эффективное программирование");
+        Book thinkingInJava = new Book(400, "Философия Java");
+        Book cleanCode = new Book(250, "Clean code");
+        Book[] books = {headFirst, effectiveJava, thinkingInJava, cleanCode};
+        for (int index = 0; index < books.length; index++) {
+            System.out.println(books[index].getName() + " - " + books[index].getPages());
+        }
+        Book tempBook = books[0];
+        books[0] = books[3];
+        books[3] = tempBook;
+        System.out.println();
+        for (int index = 0; index < books.length; index++) {
+            System.out.println(books[index].getName() + " - " + books[index].getPages());
+        }
+        System.out.println();
+        for (int index = 0; index < books.length; index++) {
+            if (books[index].getName().equals("Clean code")) {
+                System.out.println(books[index].getName() + " - " + books[index].getPages());
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Store.java
@@ -0,0 +1,9 @@
+package ru.job4j.pojo;
+
+public class Store {
+    public static void main(String[] args) {
+        Product milk = new Product("Milk", 10);
+        Product bread = new Product("Bread", 4);
+        Product egg = new Product("Egg", 19);
+    }
+}
