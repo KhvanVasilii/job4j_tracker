78377b6f08710d61c8a0c3cc19e36f328f29502e
KhvanVasilii <hvanvasya@gmail.com>
2022-08-28 10:16:48 +0900

    Тестирование. Подготовка данных. [#182960 #292631]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -42,6 +42,6 @@ public class StartUITest {
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(null, is(deleted));
+        assertNull(deleted);
     }
 }
